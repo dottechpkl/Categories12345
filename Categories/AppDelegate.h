7e5c9319ee8a64493e08fffd8d7f2df29a975278
Dottechnologies <pchandra@DOTmac3.local>
2014-05-14 10:29:56 +0530

    Initial Commit

---
+++ Categories/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Categories
+//
+//  Created by Dottechnologies on 5/14/14.
+//  Copyright (c) 2014 Dottechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Categories/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Categories
+//
+//  Created by Dottechnologies on 5/14/14.
+//  Copyright (c) 2014 Dottechnologies. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
